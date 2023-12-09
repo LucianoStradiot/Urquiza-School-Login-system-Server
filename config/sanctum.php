04629c63d93c9dc1204a6d60b6571b19bb3b21aa
unknown <lstradiot15@gmail.com>
2023-12-08 22:27:37 -0300

    change the routes to try if the deploy works

--- config/sanctum.php
+++ config/sanctum.php
@@ -32,7 +32,6 @@ return [
     | token that's present on an incoming request for authentication.
     |
     */
-    'prefix' => 'api',
     'guard' => ['web'],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +19,17 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/super-admin', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/alumno', function (Request $request) {
+        return $request->user();
+    });
+});
+Route::post('/signup/super-admin', [AuthController::class, 'signupSuperAdmin']);
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+
+require __DIR__ . '/auth.php';
