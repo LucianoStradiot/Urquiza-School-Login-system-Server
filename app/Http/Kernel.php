18f754d0ea20ac3baa3f8bf9e3c472bb7b7d5bf2
unknown <lstradiot15@gmail.com>
2023-12-11 17:07:02 -0300

    Added a few changes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,9 +67,4 @@ class Kernel extends HttpKernel
         'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
     ];
 
-    protected $routeMiddleware = [
-        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
-        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
-        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
-    ];
 }
