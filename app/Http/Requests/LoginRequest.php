0b464fe257d1cb538d521f06fadaa5cc92b6c8ed
juliancantons <cantonsjulian@gmail.com>
2023-12-19 20:13:19 -0300

    Email and password validation

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -11,13 +11,17 @@ class LoginRequest extends FormRequest
         return true;
     }
 
+
+
     public function rules(): array
     {
         return [
-            'email' => 'required|email',
-            'password' => 'required',
+            'email' => ['required', 'email', 'regex:/^[A-Za-z]+@terciariourquiza\.edu\.ar$|^[0-9]+@terciariourquiza\.edu\.ar$/'],
+
+            'password' => ['required', 'regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/'],
         ];
     }
+
 
     public function withValidator($validator)
     {
@@ -30,8 +34,26 @@ class LoginRequest extends FormRequest
         });
     }
 
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'El correo electrónico es obligatorio.',
+            'email.email' => 'Por favor, ingresa una dirección de correo electrónico válida.',
+            'email.regex' => 'El correo electrónico ingresado no respeta el formato correcto.',
+            'email.no_spaces' => 'El correo electrónico no puede contener espacios en blanco.',
+            'password.required' => 'La contraseña es obligatoria.',
+            'password.regex' => 'La contraseña debe tener al menos 8 caracteres y contener al menos una letra mayúscula, una letra minúscula y un número.',
+            'password.no_spaces' => 'La contraseña no puede contener espacios en blanco.',
+
+        ];
+    }
+
+
+
     protected function validateExists($value, $table)
     {
         return \DB::table($table)->where('email', $value)->exists();
     }
 }
+
